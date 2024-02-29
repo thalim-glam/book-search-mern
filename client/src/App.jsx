c86d3ab5d5ea10b33c0ea411938eb441698584bf
Tasneem <thalim.glam@gmail.com>
2024-02-29 12:11:10 -0800

    Changing app jsx to update into graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,22 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Navbar />
+        <Outlet />
+      </div>
+    </ApolloProvider>
   );
 }
 
