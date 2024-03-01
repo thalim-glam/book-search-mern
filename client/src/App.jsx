d01596ea6154d6cd2a12261c4ea495bd25f541f7
Tasneem <thalim.glam@gmail.com>
2024-03-01 13:12:52 -0800

    Rewriting app jsx for testing purpose

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,34 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Navbar from './components/Navbar';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
-  cache: new InMemoryCache(),
 });
 
-import Navbar from './components/Navbar';
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Navbar />
-        <Outlet />
-      </div>
+      <Navbar />
+      <Outlet />
+
     </ApolloProvider>
   );
 }
