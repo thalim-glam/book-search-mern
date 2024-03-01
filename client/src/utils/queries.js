77831171125d74f3aac6285bd162ef8d78f4f7eb
Tasneem <thalim.glam@gmail.com>
2024-03-01 12:34:28 -0800

    Did the code review and changed all the files in client utils.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,22 +1,22 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_TECH = gql`
-  query tech {
-    tech {
+export const GET_ME = gql`
+  query me {
+    me {
       _id
-      name
+      username
+      email
+      bookCount
+      savedBooks{
+        authors
+        description
+        title
+        bookId
+        image
+        link
+      }
     }
   }
 `;
 
-export const QUERY_MATCHUPS = gql`
-  query matchups($_id: String) {
-    matchups(_id: $_id) {
-      _id
-      tech1
-      tech2
-      tech1_votes
-      tech2_votes
-    }
-  }
-`;
+
